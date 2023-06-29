3ee85eafe6a06febf042e734de2677d7a03589b9
Makhmud <mahmudnaimjonov9@gmail.com>
2023-06-29 22:54:21 +0500

    first commit

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: 'Ruco Fu Pro Drone',
+        productNumber: '49347',
+        productStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Ruco Fu Pro Drone',
+        productNumber: '49347',
+        productStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber: '969996',
+        productStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'GPS 4k Drone',
+        productNumber: '22821',
+        productStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Ruco Fu Pro Drone',
+        productNumber: '49347',
+        productStatus: 'Due',
+        shipping: 'Pending'
+    }
+]
